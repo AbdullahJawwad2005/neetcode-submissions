242b26316302ccf238faddf978207186bba935b6
Abdullah Jawwad Yousafi <yousafiaj@beloit.edu>
2026-05-31 20:01:57 -0500

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.py
@@ -0,0 +1,40 @@
+class TreeNode:
+        def __init__(self):
+            self.characters = {}
+            self.end = False
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = TreeNode()
+        
+    def insert(self, word: str) -> None:
+        curr = self.root
+
+        for ch in word:
+            if ch not in curr.characters:
+                curr.characters[ch] = TreeNode()
+            curr = curr.characters[ch]
+        curr.end = True
+
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for ch in word:
+            print(curr.end)
+            if ch not in curr.characters:
+                return False
+            curr = curr.characters[ch]
+        if curr.end == True:
+            return True
+        return False
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for ch in prefix:
+            if ch not in curr.characters:
+                return False
+            curr = curr.characters[ch]
+        return True
+        
+        
