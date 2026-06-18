f7b7c10074157e42bf9727581e0b0f2f9f3596a3
Abdullah Jawwad Yousafi <yousafiaj@beloit.edu>
2026-06-17 22:39:42 -0400

    Add: maximum-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # kadanes algorithm
+        #
+        # [-2, -3, 4, 5, -9, 10]
+        #   [-2, -3, 4, 5, -9, 1]
+        cur_sum = 0
+        max_sum = float('-inf')
+
+        for i in range(len(nums)):
+            cur_sum += nums[i]
+            max_sum = max(cur_sum, max_sum)
+            
+
+            if cur_sum < 0:
+                cur_sum = 0
+        return max_sum
